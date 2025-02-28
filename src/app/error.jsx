cc0ed180fdb44b09951d63a49dbd23f42c58abb0
kyawgyi-2024 <letspeakenglish99@gmail.com>
2025-02-28 16:54:53 +0630

    Detail Id fixed

---
+++ src/app/error.jsx
@@ -0,0 +1,35 @@
+"use client"
+
+import Link from "next/link";
+// import { useRouter } from "next/router";
+// import React, { useEffect } from "react";
+
+const error = () => {
+  // const router = useRouter();
+
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     router.push("/");
+  //   }, 5000);
+
+  //   return () => clearTimeout(timer);
+  // }, []);
+  return (
+    <div className=" flex flex-col items-center justify-center mt-60 mb-40">
+      <h1 className=" font-bold font-title text-blue-600 text-9xl">404</h1>
+      <h6 className=" text-5xl font-bold font-title text-slate-600">
+        <span className=" font-title text-red-600">Oops!</span> Page not found.
+      </h6>
+      <p className=" text-slate-500 mt-3 mb-5">
+        {"The page you're looking for doesn't exist"}
+      </p>
+      <Link href="/">
+        <span className=" bg-slate-400 text-blue-800 mt-10 font-semibold font-title px-6 py-2 text-sm rounded">
+          Go to Home!
+        </span>
+      </Link>
+    </div>
+  );
+};
+
+export default error;
--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -1,31 +1,24 @@
 import Meta from "@/components/Meta";
 import Image from "next/image";
 
-import axios from "axios";
-import { server } from "@/config";
-
-export async function generateStaticParams() {
-  
-  const res = await axios(
-    `${server}/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
-  );
-  const movies = res.data.results;
-  const ids = movies.map((movie) => movie.id);
-
-  return ids.map((id) => ({
-    id: id.toString(),
-  }));
-}
-
-async function getMovie(id) {
-  const res = await axios(
-    `${server}/movie/${id}?api_key=${process.env.API_KEY}&language=en-US`
-  );
-  return res.data;
-}
 
 const page = async ({ params }) => {
-  const movie = await getMovie(params.id);
+  // console.log('Params:', params);
+
+  const { id } = params;
+  // console.log('ID:', id);
+
+  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}&language=en-US`;
+  // console.log('Request URL:', url);
+
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    console.error('Failed to fetch movie details:', res.status, res.statusText);
+    return <p>Error loading movie details. Status: {res.status}</p>;
+  }
+
+  const movie = await res.json();
   return (
     <div className=" container mx-auto max-w-4xl pt-6">
       <Meta title={movie.title} />
